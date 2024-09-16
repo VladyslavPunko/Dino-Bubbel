0529355c938c77b2b7da99b85e7560ef95be3823
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-09-16 22:20:47 +0200

    faq

--- src/main.js
+++ src/main.js
@@ -9,3 +9,21 @@ burger.addEventListener('click', () => {
 close.addEventListener('click', () => {
   header.classList.remove('open');
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const questions = document.querySelectorAll('.faq-question');
+
+  questions.forEach(question => {
+    question.addEventListener('click', () => {
+      const answer = question.nextElementSibling;
+
+      if (answer.style.maxHeight) {
+        answer.style.maxHeight = null;
+      } else {
+        answer.style.maxHeight = answer.scrollHeight + 'px';
+      }
+
+      question.classList.toggle('active');
+    });
+  });
+});
